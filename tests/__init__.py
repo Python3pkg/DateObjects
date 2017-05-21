958179bd188d6f8ab4d5e886d8c391c809816422
Python3pkg <raliclo@gmail.com>
2017-05-21 05:45:09 -0700

    Convert to Python3

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,8 @@
 import unittest
 
-from delta_dates import *
-from delta_operations import *
-from part_of_dates import *
+from .delta_dates import *
+from .delta_operations import *
+from .part_of_dates import *
 
 
 if __name__ == '__main__':
